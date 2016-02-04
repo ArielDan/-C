d85c5f16c20bea8bd4d30f19827287cd05168566
Ariel <xdanealine@gmail.com>
2016-02-04 22:58:05 +0800

    函数指针

---
+++ 指向函数的指针/源.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int sum(int x, int y);
+
+void main(){
+	int (*p)(int ,int );	//定义指向函数的指针变量
+	int a, b, c;
+	p = sum;	//sum 函数的入口地址赋给指针变量p
+	scanf("%d %d", &a, &b);
+	c = (*p)( a,b);	//用指向函数的指针变量调用函数
+	printf("a=%d,b=%d,sum=%d", a, b, c);
+
+}
+
+int sum(int x, int y){
+	int z;
+	z = x + y;
+	return z;
+
+}
